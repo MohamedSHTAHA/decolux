cd13828c8f60acd5817b435ea83d0a88f08f68f7
Mohamed Shaban <mohamed.taha@appssquare.com>
2021-07-01 01:42:50 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,7 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right "></i></a>
+                <a href="{{route('front.job-openings.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right "></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -44,14 +44,14 @@
             <!-- small box -->
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                    <h3>{{\App\Project::count()}}</h3>
 
-                    <p>Bounce Rate</p>
+                    <p>Projects</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{route('front.portfolios')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->

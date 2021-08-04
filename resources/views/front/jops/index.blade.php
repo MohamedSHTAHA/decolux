44662cfc0ecfcb022aa4a74aebbf0707121b6ca9
mohamed.sh.taha2015 <mohamed.sh.taha2015@gmail.com>
2021-08-04 03:10:09 +0200

    new updates

--- resources/views/front/jops/index.blade.php
+++ resources/views/front/jops/index.blade.php
@@ -80,7 +80,7 @@
                                                     <tr class="thjmf-listing-body">
                                                         <td class="thjmf-job-single-excerpt">
                                                             <div class="thjmf-listing-single-content">
-                                                                <p>{!! $jop->description !!}
+                                                                <p>{!! str_split(strip_tags($jop->description),50) !!}
                                                                 </p>
                                                             </div>
                                                         </td>

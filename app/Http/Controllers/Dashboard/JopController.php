a87351dfe754c8044f46cd064540b5334d588046
mohamed.sh.taha2015 <mohamed.sh.taha2015@gmail.com>
2021-08-04 02:16:44 +0200

    new updates applies with out jop

--- app/Http/Controllers/Dashboard/JopController.php
+++ app/Http/Controllers/Dashboard/JopController.php
@@ -16,31 +16,33 @@ class JopController extends Controller
     public function index(Request $request)
     {
         $jops = Jop::when($request->search, function ($q) use ($request) {
-            return $q->where('title','like', '%' . $request->search . '%')->orWhere('description','like', '%' . $request->search . '%');
+            return $q->where('title', 'like', '%' . $request->search . '%')->orWhere('description', 'like', '%' . $request->search . '%');
         })->latest()->paginate(10);
 
         return view('dashboard.jops.index', compact(['jops']));
     }
 
-    public function applies(Request $request , $id)
+    public function applies(Request $request, $id)
     {
         $applies = Apply::whereJopId($id)->when($request->search, function ($q) use ($request) {
-            return $q->where('firstname', 'like','%' . $request->search . '%')
-                ->orWhere('lastname', 'like','%' . $request->search . '%')
-                ->orWhere('email', 'like','%' . $request->search . '%')
-                ->orWhere('phone', 'like','%' . $request->search . '%');
+            return $q->where('firstname', 'like', '%' . $request->search . '%')
+                ->orWhere('lastname', 'like', '%' . $request->search . '%')
+                ->orWhere('email', 'like', '%' . $request->search . '%')
+                ->orWhere('phone', 'like', '%' . $request->search . '%');
         })->latest()->paginate(10);
 
         return view('dashboard.jops.applies', compact(['applies']));
     }
 
-    public function appliesFree(Request $request )
+    public function appliesFree(Request $request)
     {
         $applies = Apply::whereNull('jop_id')->when($request->search, function ($q) use ($request) {
-            return $q->where('firstname', 'like','%' . $request->search . '%')
-                ->orWhere('lastname', 'like','%' . $request->search . '%')
-                ->orWhere('email', 'like','%' . $request->search . '%')
-                ->orWhere('phone', 'like','%' . $request->search . '%');
+            return $q->where(function ($query) use ($request) {
+                $query->where('firstname', 'like', '%' . $request->search . '%')
+                    ->orWhere('lastname', 'like', '%' . $request->search . '%')
+                    ->orWhere('email', 'like', '%' . $request->search . '%')
+                    ->orWhere('phone', 'like', '%' . $request->search . '%');
+            });
         })->latest()->paginate(10);
 
         return view('dashboard.jops.appliesFree', compact(['applies']));

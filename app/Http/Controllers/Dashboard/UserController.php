6bb0822e96f04245005a0c420e2ca4b0b01916ab
mohamed.sh.taha2015 <mohamed.sh.taha2015@gmail.com>
2021-08-07 00:16:15 +0200

    new updates UserController

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
 
     public function edit(User  $user)
     {
-        return view('dashboard.users.edit');
+        return view('dashboard.users.edit',compact('user'));
     }
 
     /**
